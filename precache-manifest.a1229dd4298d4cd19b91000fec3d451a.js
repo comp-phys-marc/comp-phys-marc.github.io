b0ca8de89ccc9fce51c156250f48583930980123
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2021-11-23 20:20:08 -0800

    Init November 2021

---
+++ precache-manifest.a1229dd4298d4cd19b91000fec3d451a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63741928ce8aee49a89e",
+    "url": "/static/css/main.bf775af2.chunk.css"
+  },
+  {
+    "revision": "63741928ce8aee49a89e",
+    "url": "/static/js/main.d27a88db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6008aac8c4c62ef9168",
+    "url": "/static/css/2.a29bad52.chunk.css"
+  },
+  {
+    "revision": "f6008aac8c4c62ef9168",
+    "url": "/static/js/2.1a0efdb0.chunk.js"
+  },
+  {
+    "revision": "dd2bf83cc46cc006d92b3fe1753f6019",
+    "url": "/index.html"
+  }
+];
