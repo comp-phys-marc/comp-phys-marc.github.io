281c3fa36d8819d0c17b400bfddc5666e51e2a6c
Marcus Edwards <edwardsmarc968@gmail.com>
2022-08-28 22:08:02 -0700

    Add open source

---
+++ precache-manifest.de2acab3d322fc799912b01623c6ccc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31786422217ee2be7762",
+    "url": "/static/css/main.bf775af2.chunk.css"
+  },
+  {
+    "revision": "31786422217ee2be7762",
+    "url": "/static/js/main.18c0f683.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6008aac8c4c62ef9168",
+    "url": "/static/css/2.a29bad52.chunk.css"
+  },
+  {
+    "revision": "f6008aac8c4c62ef9168",
+    "url": "/static/js/2.1a0efdb0.chunk.js"
+  },
+  {
+    "revision": "bbb9a74d2a48d2d96842639554af3207",
+    "url": "/index.html"
+  }
+];
